adc70473848ec7d67620b3d02a6d13a652895156
Lloyd Maniquiz <lloydmaniquiz@gmail.com>
2021-02-21 22:28:02 +0800

    some changes on buttons

--- Assets/Resources/Scripts/MainMenu.cs
+++ Assets/Resources/Scripts/MainMenu.cs
@@ -5,13 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void Shop()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene(3);
     }
 }
