d3a5defe51e1bac5d9d8db16d9acbbbbcc943bf7
mmtapalla <gm.tapallamiguel@gmail.com>
2021-02-23 02:55:44 +0800

    added splash screen via newly made Intro scene

---
+++ Assets/Resources/Scripts/intro.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class intro : MonoBehaviour
+{
+
+    public float wait_time = 7f;
+
+
+    void Start()
+    {
+        StartCoroutine(intro_duration());
+    }
+
+    IEnumerator intro_duration()
+    {
+        yield return new WaitForSeconds(wait_time);
+
+        SceneManager.LoadScene(0);
+    }
+
+}
